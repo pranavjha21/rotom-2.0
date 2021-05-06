59580f6f3ec003be17a37eab1c1ef2ed4a9048da
Pranav <77088825+pranavjha21@users.noreply.github.com>
2021-05-06 10:49:34 +0530

    Update config.py
    Removed sensible things

--- src/Config.py
+++ src/Config.py
@@ -1,9 +1,9 @@
 import os
 
 class Config:
-    aid = int(os.environ.get("API_ID", "2327886"))
-    ahash = os.environ.get("API_HASH", "b27fc6b63c22a6917438a57bc616075c")
-    bot_token = os.environ.get("BOT_TOKEN", "1392937433:AAFj4L8wHUfeWG94iGrxSH3ZNh5fkbEroL0")
+    aid = int(os.environ.get("API_ID", "entervid"))
+    ahash = os.environ.get("API_HASH", "enter api hash")
+    bot_token = os.environ.get("BOT_TOKEN", "enter token)
     sudo = [1078841825, 1076632911]
     # try:
     #     sudo = set(int(x) for x in os.environ.get("SUDO", "").split(','))
